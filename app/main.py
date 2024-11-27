d227963d0bca12d884a488b36aaf27dc27bea2ca
Leon <100690100+LeonDeTur@users.noreply.github.com>
2024-11-27 12:04:46 +0300

    feat(prioc): added prioc services and common services

---
+++ app/main.py
@@ -0,0 +1,30 @@
+import sys
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from loguru import logger
+
+from .common.config import config
+from .routers import prioc_router
+
+
+logger.remove()
+logger.add(
+    sys.stdout,
+    format="<green>{time:MM-DD HH:mm}</green> | <level>{level:<8}</level> | <cyan>{message}</cyan>",
+    level="INFO",
+    colorize=True
+)
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(prioc_router, prefix=config.get("FASTAPI_PREFIX"))
---
+++ app/routers/__init__.py
@@ -0,0 +1,1 @@
+from .prioc import prioc_router
